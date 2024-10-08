01c5c9ff7c15dd8732c6f5709b0622e3b4b41299
Temaghb <artemfaxrutdinov.2018@gmail.com>
2024-10-08 22:50:22 +0300

    Add files via upload

---
+++ основы алгоритмизации/основы алгоритмизации/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace YourNamespace
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenTranslator(object sender, RoutedEventArgs e)
+        {
+            TranslatorWindow translatorWindow = new TranslatorWindow();
+            translatorWindow.Show();
+        }
+
+        private void OpenCalculator(object sender, RoutedEventArgs e)
+        {
+            CalculatorWindow calculatorWindow = new CalculatorWindow();
+            calculatorWindow.Show();
+        }
+
+        private void OpenIntervalDefiner(object sender, RoutedEventArgs e)
+        {
+            IntervalDefinerWindow rangeCheckerWindow = new IntervalDefinerWindow();
+            rangeCheckerWindow.Show();
+        }
+
+        private void OpenPasswordChecker(object sender, RoutedEventArgs e)
+        {
+            PasswordCheckerWindow passwordCheckerWindow = new PasswordCheckerWindow();
+            passwordCheckerWindow.Show();
+        }
+    }
+}
---
+++ основы алгоритмизации/основы алгоритмизации/PasswordChecker.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace YourNamespace
+{
+    public partial class PasswordCheckerWindow : Window
+    {
+        private string correctPassword = "PracticaPassword";
+
+        public PasswordCheckerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckPassword(object sender, RoutedEventArgs e)
+        {
+            if (PasswordTextBox.Text == correctPassword)
+            {
+                ResultTextBlock.Text = "Доступ разрешен!";
+            }
+            else
+            {
+                ResultTextBlock.Text = "Неверный пароль. Попробуйте ещё раз.";
+            }
+        }
+    }
+}
